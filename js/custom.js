e2750622f677a16d92ec4f21f9e35e36896fb041
edvigindi <edvigindi@gmail.com>
2019-11-18 20:35:41 +0200

    with css

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  
+    $('.sidenav').sidenav();
+    $('.scrollspy').scrollSpy();
+
+    /* $('.scroll-down-buton i').click (function() {
+      console.log($('.scroll-down-buton i'));
+      $('html, body').animate({scrollTop: $('section#test2').offset().top }, 'slow');
+      return false;
+    }); */
+    $(document).on('click','.fa-arrow-alt-circle-down',function(){
+      console.log($('i'));
+      $('html, body').animate({scrollTop: $('section#test2').offset().top - 120}, 'slow');
+    });
+    $('select').formSelect();
+    $('input#input_text, textarea#textarea2').characterCounter();
+});
